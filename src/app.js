ffdd4daa6b5aaf06c750e2934f07a2b662f7978a
seetharam <seetharam.vanamareddi@outlook.com>
2021-03-19 10:55:05 -0400

    coffeewebsite

---
+++ src/app.js
@@ -0,0 +1,18 @@
+const sideNav = document.querySelector(".sidenav");
+M.Sidenav.init(sideNav, {});
+
+const carousel = document.querySelector(".carousel");
+M.Carousel.init(carousel, {
+	fullWidth: true,
+});
+
+const parallax = document.querySelector(".parallax");
+M.Parallax.init(parallax, {});
+
+
+// const myReviews = document.querySelector(".carousel");
+// M.Carousel.init(carousel, {
+// 	numVisible: 7,
+// 	shift: 50,
+// 	padding: 50,
+// });
